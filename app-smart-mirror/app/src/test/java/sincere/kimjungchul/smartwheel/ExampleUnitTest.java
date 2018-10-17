36c5b1b213e412ffc75f7a9144519081bed67f19
YiSuBin <tnqls5893@naver.com>
2018-10-17 15:51:04 +0900

    smart-mirror

---
+++ app-smart-mirror/app/src/test/java/sincere/kimjungchul/smartwheel/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package sincere.kimjungchul.smartwheel;
+//package softwarek.kimjungchul.smartmirror;
+//package softwarek.kimjungchul.smartmirror;
+import static junit.framework.Assert.assertEquals;
+//package com.example.a223.smartmirror;
+
+//import org.junit.Test;
+
+//import static junit.framework.Assert.assertEquals;
+//import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+   // @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
